e211f92478ef02ec72b4ef997f1876c8f86278ec
Wiaczeslaw <105964596+vkubarevc@users.noreply.github.com>
2022-05-21 01:05:26 -0700

    Create smga.cpp

---
+++ smga.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+int main ()
+{
+	double num, sum=0;
+	int counter=0;
+	in >> num; 
+	
+	ifstream in;
+	in.open("output.dat");
+	ofstream out;
+	out.open("output.dat");
+	
+while(num !=-99)
+
+ {
+	 cout << num << endl;  
+	 out << num << endl;
+	 sum+=num;
+	 counter++;
+	 in >> num;
+ }
+ 
+	cout << "average = " << sum/10 << endl;
+	out << "average = " << sum/10 << endl;
+
+in.close();
+out.close();
+
+return 0;
+}
