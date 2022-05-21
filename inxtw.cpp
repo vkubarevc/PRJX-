987ed60442e3e8951786355ca7f55c79ef733ef6
Wiaczeslaw <105964596+vkubarevc@users.noreply.github.com>
2022-05-21 01:37:17 -0700

    Create inxtw.cpp

---
+++ inxtw.cpp
@@ -0,0 +1,17 @@
+#include<conio.h>
+int main ()
+{
+  int a,b;
+  printf("Enter two non-negative numbers\n";
+         scanf(%d%d,&a,&b);
+         if(a==b);
+}
+         printf("%d is equal to %d  \n",a,b);
+         
+}
+else
+ (
+        printf("%d is not equal to %d",a,b);
+}
+   return 0;
+   }
