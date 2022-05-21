31bc29163e2578b6e830e2fe93f665bba92014f6
Wiaczeslaw <105964596+vkubarevc@users.noreply.github.com>
2022-05-21 00:51:29 -0700

    Create arc.cpp

---
+++ arc.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+int main()
+
+{
+
+      int l,w, L, W, area1, area2;
+cout <<"Enter the length and width of the first rectangle.\n";
+cin >> l;
+cin >> w;
+cout <<"Enter the length and width of the second rectangle.\n";
+cin >> L;
+cin >> W;
+area1=l*w
+cout <<"The area of the first rectangle is << area1 << endl;
+area2=L*W
+cout <<"The area of the second rectangle is << area2 << endl;
+
+if (area1 > area2){
+	cout <<"The first area is larger than the second.\n";
+else (area2 > area2) 
+	cout <<"The second area is larger than the first.\n";
+}
+
+return 0;
+}
